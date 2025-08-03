d18601e4ed458bd7ee266f8e65f9939d6c015834
Blackspider2003 <mdbishalahmedrafi55@gmail.com>
2025-08-03 17:43:23 +0600

    SOme bugs fixed

--- src/app/api/my-bookings/[id]/route.js
+++ src/app/api/my-bookings/[id]/route.js
@@ -1,5 +1,7 @@
+import { authOptions } from "@/lib/authOptions"
 import dbConnect, { collectionNameObj } from "@/lib/dbConnect"
 import { ObjectId } from "mongodb"
+import { getServerSession } from "next-auth"
 import { revalidatePath } from "next/cache"
 import { NextResponse } from "next/server"
 
@@ -7,16 +9,34 @@ export const GET = async (req, {params}) => {
                 const singleItem = await params
                 const bookingCollection = dbConnect(collectionNameObj.bookingCollection)
                 const query = {_id: new ObjectId(singleItem.id)}
-                const singleBooking = await bookingCollection.findOne(query)
 
-                return NextResponse.json(singleBooking)
+                   const session = await getServerSession(authOptions)
+                const email = session?.user?.email
+                const currentBookingData = await bookingCollection.findOne(query)
+                const  isUserOk = email === currentBookingData?.email
+                if(isUserOk) { 
+           return NextResponse.json(singleBooking)
+                }
+                else{ 
+                              return NextResponse.json({message: "Forbidden GET action"}, { 
+                                                status: 403
+                                })   
+                }
+               
 }
 
 export const PATCH = async (req, {params}) => { 
                  const singleItem = await params
                 const bookingCollection = dbConnect(collectionNameObj.bookingCollection)
                 const query = {_id: new ObjectId(singleItem.id)}
-                const body = await req.json()
+
+                const session = await getServerSession(authOptions)
+                const email = session?.user?.email
+                const currentBookingData = await bookingCollection.findOne(query)
+                const  isUserOk = email === currentBookingData?.email
+
+                if(isUserOk) { 
+                                 const body = await req.json()
                 const filter = { 
                                 $set: { ...body }
                 }
@@ -26,4 +46,11 @@ export const PATCH = async (req, {params}) => {
                 const updateResponse = await bookingCollection.updateOne(query,filter, option)
                 revalidatePath("/my-bookings")
                 return NextResponse.json(updateResponse)
+                }
+                else{ 
+                                return NextResponse.json({message: "Forbidden Update action"}, { 
+                                                status: 403
+                                })
+                }
+               
 }
--- src/app/my-bookings/[id]/page.jsx
+++ src/app/my-bookings/[id]/page.jsx
@@ -1,13 +1,25 @@
 
 import BookingUpdateForm from '@/Components/fomrs/BookingUpdateForm';
+import dbConnect, { collectionNameObj } from '@/lib/dbConnect';
+import { ObjectId } from 'mongodb';
+import { headers } from 'next/headers';
 import React from 'react';
 export default async function UpdateBookingPage({params}) {
                 const p = await params;
-                const res = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`)
-                const data = await res.json()
+                console.log(p);
+                // const res = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`, { 
+                //                 headers: await headers(),
+                // });
+                const bookingCollection = dbConnect(collectionNameObj.bookingCollection)
+const singleItem = await bookingCollection.findOne({_id: new ObjectId(p.id)})
+console.log(singleItem);
+                // const data = await res.json()
   return (
     <div>
-                <BookingUpdateForm data={data}></BookingUpdateForm>
+                <BookingUpdateForm data={singleItem}></BookingUpdateForm>
     </div>
   );
 }
+
+
+
