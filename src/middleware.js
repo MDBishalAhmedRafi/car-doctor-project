4891071590c40c7f903702ee4847c5e165f6e1dd
Blackspider2003 <mdbishalahmedrafi55@gmail.com>
2025-08-04 13:52:41 +0600

    some issue fixed

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,20 @@
-import { getToken } from "next-auth/jwt"
-import { NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
-export const middleware = async (req) => { 
-                const token = await getToken({req})
-                if(token) { 
-                                return NextResponse.next()
-                }
-                else{ 
-                                return NextResponse(new URL('/login', req.url))
-                }
-               
-                return NextResponse.next()
-}
+export const middleware = async (req) => {
+  const token = await getToken({ req });
 
-export const config = { 
-                matcher: ['/my-bookings', 
-                                '/my-bookings/:path*', 
-                '/checkout/:path*'
-                ],
-}
+  if (token) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL('/login', req.url)); // ✅ FIXED
+  }
+};
+
+export const config = {
+  matcher: [
+    '/my-bookings',
+    '/my-bookings/:path*',
+    '/checkout/:path*',
+  ],
+};
